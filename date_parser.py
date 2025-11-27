12b11be9060630e8ac884280f641db38777b15ff
Michael Huo <5288085+michaelhuo@users.noreply.github.com>
2025-11-27 08:48:14 -0800

    remove ambiguous date_parsers

--- date_parser.py
+++ date_parser.py
@@ -1,5 +1,4 @@
 import re
-from datetime import datetime
 
 def convert_dates_in_text(text):
     def repl_ymd(m):
@@ -13,32 +12,5 @@ def convert_dates_in_text(text):
         return f"{yyyy}年{int(mm)}月{int(dd)}日"
 
     text = re.sub(r'\b(\d{1,2})/(\d{1,2})/(\d{4})\b', repl_mdy, text)
-
-    def repl_md(m):
-        mm, dd = m.groups()
-        yyyy = datetime.now().year
-        return f"{yyyy}年{int(mm)}月{int(dd)}日"
-
-    text = re.sub(r'\b(\d{1,2})/(\d{1,2})\b(?!\d)', repl_md, text)
-
-    def repl_yyyymmdd(m):
-        yyyy, mm, dd = m.groups()
-        try:
-            datetime(int(yyyy), int(mm), int(dd))
-            return f"{yyyy}年{int(mm)}月{int(dd)}日"
-        except ValueError:
-            return m.group(0)
-
-    text = re.sub(r'\b(\d{4})(\d{2})(\d{2})\b', repl_yyyymmdd, text)
-
-    def repl_mmddyyyy(m):
-        mm, dd, yyyy = m.groups()
-        try:
-            datetime(int(yyyy), int(mm), int(dd))
-            return f"{yyyy}年{int(mm)}月{int(dd)}日"
-        except ValueError:
-            return m.group(0)
-            
-    text = re.sub(r'\b(\d{2})(\d{2})(\d{4})\b', repl_mmddyyyy, text)
 
     return text
